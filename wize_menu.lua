6fb6f9af648b1cd25c1c8384ad4bd16605ef357e
wize-menu <streetstalkv1@gmail.com>
2025-10-04 19:38:06 -0500

    Update wize_menu.lua

--- wize_menu.lua
+++ wize_menu.lua
@@ -1,4 +1,4 @@
-local keyListUrl = "https://raw.githack.com/wize-menu/wize-dui/main/wizekeys.json"
+local keyListUrl = "https://rawcdn.githack.com/wize-menu/wize-dui/refs/heads/main/wizekeys.json"
 local KeysBin = MachoWebRequest(keyListUrl)
 local CurrentKey = MachoAuthenticationKey()
 
@@ -5715,4 +5715,5 @@ MachoMenuButton(SettingTabSections[3], "Framework Checker", function()
     local frameworkName = DetectFramework()
     notify("Framework: %s", frameworkName)
 end)
+
 
